d2989d5acaa282a44c889ba43eae2a090cc85171
sieep-coding <nstamb6@gmail.com>
2024-11-24 23:27:47 -0500

    persistent storage and command flags added

--- main.go
+++ main.go
@@ -4,9 +4,7 @@ func main() {
 	todos := Todos{}
 	storage := NewStorage[Todos]("todos.json")
 	storage.Load(&todos)
-	todos.add("Buy milk")
-	todos.add("Buy milk")
-	todos.toggle(0)
-	todos.print()
+	cmdFlags := NewCmdFlags()
+	cmdFlags.Execute(&todos)
 	storage.Save(todos)
 }
